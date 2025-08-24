25033ae3091c38412f6ed6a46049b2ae9972e06a
Ben <btainleyzoll@gmail.com>
2025-08-24 15:43:42 +0100

    I created the multiplication and division options for the calculator, and I also made the code a little cleaner. My next goal is to augment this so users can put in their own calculations and it come back for them.

--- Basic_Projects/Simple_Calculator.py
+++ Basic_Projects/Simple_Calculator.py
@@ -1,8 +1,14 @@
+import sys
+
+
 print("Welcome to my Calculator, What calculation would you like to perform: \n" +
       "Addition, Subtraction, Multiplication, or Division.\n" +
       "Type A(Addition), S(Subtraction), M(Multiplication), or D(Division)")
 #This will indicate how the user wants to use the calculator
 Selection = input("Calculation type: ").lower()
+num1 = float(input("First number: "))
+        
+num2 = float(input("Second number: "))
 
 def Addition():
     '''
@@ -13,39 +19,40 @@ def Addition():
         num2(float): second number
     
     '''
-    print("Addition it is!\n")
-    num1 = float(input("First number: "))
-        
-    num2 = float(input("Second number: "))
+    print("Addition it is!")
         #Adds the two numbers that the user gave together to give them the output.
-    answer = num1 + num2
-        
+    answer = num1 + num2   
     print(answer)
         
 def Subtraction():
-    '''
-    Returns the difference of num1 and num2.
-    
-    Parameters:
-        num1(float): first number
-        num2(float): second number
-    
-    '''
-    print("Subtraction it is!\n")
-    num1 = float(input("First Number: "))
-    
-    num2 = float(input("Second number: "))
-        #Subtracts the two numbers given by the user to give an output.
+    print("Subtraction it is!")
+    #Subtracts the two numbers given by the user to give an output.
     answer = num1 - num2
     print(answer)
     
+def Multiplication():
+    print("Multiplication it is!")
+    #Multiplies the two numbers together that were given by the user,
+    answer = num1 * num2
+    print(answer)
     
-    
+def Division():
+    print("Division it is!")
+    try: 
+        answer = num1 / num2
+        print(answer)
+    except:
+        print("Cannot divide by zero!")
+        sys.exit()
     
 if Selection.lower() == 'a':
     Addition()
 elif Selection.lower() == 's': 
     Subtraction()
+elif Selection.lower() == 'm':
+    Multiplication()
+elif Selection.lower() == 'd':
+    Division()
 else:
     print ("Please choose from the options listed.")
     

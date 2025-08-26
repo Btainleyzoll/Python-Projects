6ea05d88e570f9812988249b0f755911fbd0953a
Ben <btainleyzoll@gmail.com>
2025-08-26 16:12:04 +0100

    This is my first commit for the unit converter. I just made the first function which will find out which type of unit the user wants to convert.

---
+++ Basic_Projects/unit_converter.py
@@ -0,0 +1,41 @@
+print("Welcome to my unit converter! \n" +
+      "This will be able to convert most units from one to another easily.")
+
+
+while True:
+    
+    
+    def type_of_unit():
+        '''
+        
+        This function will find out which unit type the user wants to convert
+        and will then give the appropriate output depending on their answer.
+        
+        '''
+        while True:
+            print("Choose which type of units would you like to convert: \n" +
+                "1. Length\n" +
+                "2. Weight\n" +
+                "3. Temperature")
+            type = input().strip()
+            if type == '1':
+                #dosomething
+                break
+            elif type == '2':
+                #dosomething
+                break
+            elif type == '3':
+                #dosomething
+                break
+            else:
+                print("Please choose from the options given.")
+                
+        
+        
+        
+        
+        
+        
+        
+    type_of_unit()
+          

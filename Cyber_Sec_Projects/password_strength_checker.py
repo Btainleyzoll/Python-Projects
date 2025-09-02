e5ad57d12b42702e794d13a2cd98635c86a45829
Ben <btainleyzoll@gmail.com>
2025-09-02 16:03:04 +0100

    This is a improved version where I did more checks and added checks to make sure the password isnt redundent. Things to still potentially add is a list of common words and if it is in the password, the password strength gets deducted and also check for multiple special char if they are the same.

--- Cyber_Sec_Projects/password_strength_checker.py
+++ Cyber_Sec_Projects/password_strength_checker.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from collections import Counter
 
 
 print("Welcome to my password strength checker.")
@@ -21,8 +22,12 @@ def check_password(pswd):
     special_check = "[!@#$%^&*()-_=+{}[];:,.<>?/|]"
     if len(pswd) < 8: #Checks that the password has a minimum input to be secure
         return score
-    else:
+    elif len(pswd) >= 8:
         score += 1
+        if len(pswd) >= 12:
+            score += 1
+        if len(pswd) >= 16:
+            score += 2
     num_check = re.findall("[0-9]", pswd) # finds all the numbers in the passwords
     set_num = set(num_check)
     sorted_num = sorted(set_num)
@@ -31,25 +36,46 @@ def check_password(pswd):
         if len(num_check) == len(sorted_num):
             score += 1
     
-    if any(char in special_check for char in pswd): # checks to see if the password contains a special char.
+    if re.search(special_check, pswd): # checks to see if the password contains a special char.
         score += 1
     
     if any(char in loweralpha for char in pswd) and any(char in upperalpha for char in pswd):
-        score +=1   
-    
+        lower_check = re.findall("[a-z]", pswd)
+        upper_check = re.findall("[A-Z]", pswd)
+        set_lower = set(lower_check)
+        set_upper = set(upper_check)
+        sorted_lower = sorted(set_lower)
+        sorted_upper = sorted(set_upper)
+        if len(sorted_lower) == len(lower_check) and len(sorted_upper) == len(upper_check):
+            score +=3
+        else:
+            total = len(pswd)
+            lower_counts = Counter(lower_check)
+            upper_counts = Counter(upper_check)
+            if max(lower_counts.values(), default=0) / total > 0.3:
+                score -=3
+            
+            if max(upper_counts.values(), default=0) / total > 0.3:
+                score -=3
+                    
+        score +=1 
+          
+    if score >= 4:
+        score += 3
+        
     return score 
     
 def password_strength(score):
-    if score == 0:
-        print("This password is very weak, please consider changing it. ")
-    elif score == 1:
-        print("This password is weak, please consider changing it. ")
-    elif score == 2:
-        print("This password is moderate. ")
-    elif score == 3:
-        print("This password is strong.")
-    elif score == 4:
-        print("This password is very strong.")
+    if score >=0 and score <= 2:
+        print("This password is very weak, try using more characters and special characters. ")
+    elif score >=3 and score <= 5:
+        print("This password is moderately weak, please consider changing it. ")
+    elif score >= 6 and score <= 8:
+        print("This password is moderately strong. ")
+    elif score <= 8:
+        print("This password is very strong, good job. ")
+    elif score <0:
+        print("This password is incredibly weak, please add more characters and special characters and numbers. ")
     
 def another_test():
     while True:
